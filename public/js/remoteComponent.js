a484f96c9848dfe1ab8a3608371a794fd2687882
Jared Wong <techlover10@gmail.com>
2017-10-04 22:06:24 -0400

    trigger ads now working, backgrounds not working yet

--- public/js/remoteComponent.js
+++ public/js/remoteComponent.js
@@ -4,22 +4,49 @@
 window.main = new Vue({
     el: '#main',
     data: {
-        client: true,
+        client: null,
         requestedSID: 3000
     },
-    created: function (){
-        // init a deck of 10 cards
-        init(10);
-
-        // register phone connection
-        socket.emit('phone-connect', this.requestedSID);
-        this.clientLoop();
+    created: function(){
+        var self = this;
+        setTimeout(function () {
+            self.setUser(true);
+        }, 5); // hacky fix for touch not working
     },
     methods: {
+        setUser: function (isClient) {
+            this.client = isClient;
+
+            document.getElementById("start").remove();
+            document.getElementById(isClient ? "table" : "deck").remove();
+            $(isClient ? "#deck" : "#table").show();
+            if (isClient) {
+                // init a deck of 10 cards
+                init(10);
+
+                // register phone connection
+                socket.emit('phone-connect', this.requestedSID);
+                this.clientLoop();
+            }
+            else {
+
+                socket.emit('table-connect', this.requestedSID);
+                // and the URL
+                document.getElementById("url").innerHTML = this.requestedSID;
+                this.tableLoop();
+            }
+        },
         clientLoop: function () {
             // init touch events in phone
             var touchTrack = new TouchTrack();
             touchTrack.init(document.getElementById("touchHandler"), touchStart, touchMove, touchEnd);
+
         },
+        tableLoop: function () {
+            socket.on('phone-connect', phoneConnected);
+            socket.on('phone-throw-sprite', throwCard);
+            socket.on('phone-get-sprite', getCard);
+            socket.on('send-ad', sendAd);
+        }
     }
 })
